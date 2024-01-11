7a79df4994e918a7f351d9c455dcbd9c77a558b1
KROomoO <pwkc00@gmail.com>
2024-01-11 17:16:20 +0900

    feat: Add login functionality

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,14 +1,67 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Axios from "axios";
 
 function Login() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const navigate = useNavigate();
+
+    const onSubmit = (data) => {
+        Axios.post("/login", {
+            email: data.email,
+            password: data.password,
+        })
+            .then((res) => {
+                if (res.status === 200) {
+                    let accessToken = res.headers["authorization"];
+                    let refreshToken = res.headers["authorization-refresh"];
+
+                    console.log("access token :", accessToken);
+                    console.log("refresh token : ", refreshToken);
+
+                    localStorage.setItem("access", accessToken);
+                    localStorage.setItem("refresh", refreshToken);
+                }
+            })
+            .then(() => {
+                navigate("/");
+            });
+    };
+
     return (
         <div>
-            <form>
-                <label>ID :</label>
-                <input type="text" />
-                <label>PW :</label>
-                <input type="password" />
-                <button type="submit">로그인</button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    type="email"
+                    {...register("email", {
+                        required: {
+                            value: true,
+                            message: "아이디를 입력해주세요.",
+                        },
+                    })}
+                    placeholder="email"
+                    autoComplete="email"
+                />
+                <p>{errors.email?.message}</p>
+                <input
+                    type="password"
+                    {...register("password", {
+                        required: {
+                            value: true,
+                            message: "비밀번호를 입력해주세요.",
+                        },
+                    })}
+                    placeholder="password"
+                    autoComplete="password"
+                />
+                <p>{errors.password?.message}</p>
+                <input type="submit" value="로그인" />
             </form>
             <Link to={"/signup"}>회원가입</Link>
         </div>
--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -1,73 +1,134 @@
 import { useState } from "react";
 import { useForm, useWatch } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Axios from "axios";
 
 import "../css/SignUp.css";
 
-function Check({ control, name }) {
-    const asdf = useWatch({
+function Validator({ control, name, validatePasswordConfirm }) {
+    const inputData = useWatch({
         control,
         name: name,
     });
 
-    const checkEmail = asdf.length >= 8 && asdf.length <= 20;
+    const validationPatterns = {
+        emailLength: inputData.length >= 6 && inputData.length <= 35,
+        passwordLength: inputData.length >= 8 && inputData.length <= 20,
+        passwordRegex:
+            /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}[\]:;<>,.?~\\/-])[a-zA-Z\d!@#$%^&*()_+{}[\]:;<>,.?~\\/-]{8,20}$/,
+        nicknameRegex: /^[a-zA-Z0-9가-힣]{2,16}$/,
+        realnameRegex: /^[a-zA-Z가-힣]{2,}$/,
+        ageRegex: /^(1[4-9]|[2-9][0-9]|100)$/,
+    };
 
-    const checkPassword =
-        /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}[\]:;<>,.?~\\/-])[a-zA-Z\d!@#$%^&*()_+{}[\]:;<>,.?~\\/-]{8,20}$/;
+    const validationMessage = <div>필수 입력란을 확인하세요.</div>;
 
     switch (name) {
         case "email":
-            if (asdf === "") {
-                return <div>필수입력사항은 모두 입력해주세요.</div>;
+            if (inputData === "") {
+                return validationMessage;
             } else {
                 return (
-                    <div>
-                        {name}, watch: {asdf}
+                    <div
+                        className={
+                            validationPatterns.emailLength
+                                ? "checked"
+                                : "unchecked"
+                        }
+                    >
+                        이메일은 6자 이상 35자 이하로 입력하세요.
                     </div>
                 );
             }
         case "password":
-            if (asdf === "") {
-                return <div>필수입력사항은 모두 입력해주세요.</div>;
+            if (inputData === "") {
+                return validationMessage;
             } else {
                 return (
                     <div>
-                        {checkEmail ? (
-                            <div>길이 통과</div>
-                        ) : (
-                            <div>길이 미통과</div>
-                        )}
-                        {checkPassword.test(asdf) ? (
-                            <div>정규표현식 통과</div>
-                        ) : (
-                            <div>정규표현식 미통과</div>
-                        )}
+                        <div
+                            className={
+                                validationPatterns.passwordLength
+                                    ? "checked"
+                                    : "unchecked"
+                            }
+                        >
+                            비밀번호는 8자 이상 20자 이하로 입력하세요.
+                        </div>
+                        <div
+                            className={
+                                validationPatterns.passwordRegex.test(inputData)
+                                    ? "checked"
+                                    : "unchecked"
+                            }
+                        >
+                            비밀번호는 영문, 숫자, 특수문자를 포함한 8자 이상
+                            20자 이하로 입력하세요.
+                        </div>
+                    </div>
+                );
+            }
+        case "passwordConfirm":
+            if (inputData === "") {
+                return validationMessage;
+            } else {
+                return (
+                    <div
+                        className={
+                            validatePasswordConfirm(inputData)
+                                ? "checked"
+                                : "unchecked"
+                        }
+                    >
+                        {validatePasswordConfirm(inputData)
+                            ? "비밀번호가 일치 합니다."
+                            : "비밀번호가 일치하지 않습니다."}
                     </div>
                 );
             }
         case "nickname":
-            if (asdf === "") {
-                return <div>필수입력사항은 모두 입력해주세요.</div>;
+            if (inputData === "") {
+                return validationMessage;
             } else {
                 return (
-                    <div>
-                        {name}, watch: {asdf}
+                    <div
+                        className={
+                            validationPatterns.nicknameRegex.test(inputData)
+                                ? "checked"
+                                : "unchecked"
+                        }
+                    >
+                        닉네임은 2자 이상 16자 이하의 영문, 숫자, 한글로
+                        입력하세요.
                     </div>
                 );
             }
         case "realname":
-            if (asdf === "") {
-                return <div>필수입력사항은 모두 입력해주세요.</div>;
+            if (inputData === "") {
+                return validationMessage;
             } else {
                 return (
-                    <div>
-                        {name}, watch: {asdf}
+                    <div
+                        className={
+                            validationPatterns.realnameRegex.test(inputData)
+                                ? "checked"
+                                : "unchecked"
+                        }
+                    >
+                        실명은 2자 이상의 영문 또는 한글로 입력하세요.
                     </div>
                 );
             }
         case "age":
             return (
-                <div>
-                    {name}, watch: {asdf}
+                <div
+                    className={
+                        validationPatterns.ageRegex.test(inputData)
+                            ? "checked"
+                            : "unchecked"
+                    }
+                >
+                    나이는 14세 이상 100세 이하로 입력하세요.
                 </div>
             );
         default:
@@ -77,33 +138,83 @@ function Check({ control, name }) {
 
 function SignUp() {
     const [onFocusInput, setOnFocusInput] = useState("");
+    const [checkEmail, setCheckEmail] = useState(false);
+    const [checkNickname, setCheckNickname] = useState(false);
 
-    const {
-        register,
-        handleSubmit,
-        control,
-        formState: { errors },
-    } = useForm();
+    const { register, handleSubmit, control, getValues, setValue } = useForm();
+
+    const navigate = useNavigate();
 
     const onSubmit = (data) => {
-        console.log(data);
+        if (checkEmail && checkNickname && data) {
+            //서버 통신 후 메인 페이지 이동
+            Axios.post("/signup", {
+                email: data.email,
+                emailChecked: data.emailAvailable,
+                password: data.password,
+                confirmPassword: data.passwordConfirm,
+                nickname: data.nickname,
+                name: data.realname,
+                age: data.age,
+            }).then(() => {
+                navigate("/");
+            });
+        }
     };
 
-    // register pattern 정규표현식 이용해서 submit 컨트롤
+    const validatePasswordConfirm = (value) => {
+        const password = getValues("password");
+        return value === password;
+    };
+
+    const checkEmailAvaliable = () => {
+        // 서버 통신 후 중복 검사 통과 시
+        setCheckEmail(true);
+        setValue("emailAvailable", "true");
+    };
+
+    const checkNicknameAvaliable = () => {
+        // 서버 통신 후 중복 검사 통과 시
+        setCheckNickname(true);
+        setValue("nicknameAvailable", "true");
+    };
+
+    console.log(typeof getValues("emailAvailable"));
 
     return (
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <input
                     type="email"
-                    {...register("email", { required: true })}
+                    {...register("email", {
+                        required: true,
+                        minLength: {
+                            value: 6,
+                        },
+                        maxLength: {
+                            value: 35,
+                        },
+                    })}
                     placeholder="email"
                     autoComplete="email"
                     onFocus={(item) => setOnFocusInput(item.target.name)}
+                    disabled={checkEmail}
                 />
                 {onFocusInput && onFocusInput === "email" ? (
-                    <Check control={control} name={onFocusInput} />
+                    <Validator control={control} name={onFocusInput} />
                 ) : null}
+                <button
+                    type="button"
+                    onClick={checkEmailAvaliable}
+                    disabled={checkEmail}
+                >
+                    이메일중복
+                </button>
+                <input
+                    type="hidden"
+                    {...register("emailAvailable")}
+                    value={false}
+                />
                 <input
                     type="password"
                     {...register("password", {
@@ -117,31 +228,66 @@ function SignUp() {
                     onFocus={(item) => setOnFocusInput(item.target.name)}
                 />
                 {onFocusInput && onFocusInput === "password" ? (
-                    <Check control={control} name={onFocusInput} />
+                    <Validator control={control} name={onFocusInput} />
                 ) : null}
-                {errors.password && <div>{errors.password.message}</div>}
+                <input
+                    type="password"
+                    {...register("passwordConfirm", {
+                        required: true,
+                        validate: validatePasswordConfirm,
+                    })}
+                    placeholder="password confirm"
+                    autoComplete="current-password"
+                    onFocus={(item) => setOnFocusInput(item.target.name)}
+                />
+                {onFocusInput && onFocusInput === "passwordConfirm" ? (
+                    <Validator
+                        control={control}
+                        name={onFocusInput}
+                        validatePasswordConfirm={validatePasswordConfirm}
+                    />
+                ) : null}
                 <input
                     type="text"
                     {...register("nickname", {
                         required: true,
+                        pattern: {
+                            value: /^[a-zA-Z0-9가-힣]{2,16}$/,
+                        },
                         minLength: {
                             value: 2,
                         },
                         maxLength: {
-                            value: 12,
+                            value: 16,
                         },
                     })}
                     placeholder="nickname"
                     autoComplete="nickname"
                     onFocus={(item) => setOnFocusInput(item.target.name)}
+                    disabled={checkNickname}
                 />
                 {onFocusInput && onFocusInput === "nickname" ? (
-                    <Check control={control} name={onFocusInput} />
+                    <Validator control={control} name={onFocusInput} />
                 ) : null}
+                <button
+                    type="button"
+                    onClick={checkNicknameAvaliable}
+                    disabled={checkNickname}
+                >
+                    닉네임중복
+                </button>
+                <input
+                    type="hidden"
+                    {...register("nicknameAvailable")}
+                    value={false}
+                />
                 <input
                     type="text"
                     {...register("realname", {
                         required: true,
+                        pattern: {
+                            value: /^[a-zA-Z가-힣]{2,}$/,
+                        },
                         minLength: {
                             value: 2,
                         },
@@ -151,20 +297,27 @@ function SignUp() {
                     onFocus={(item) => setOnFocusInput(item.target.name)}
                 />
                 {onFocusInput && onFocusInput === "realname" ? (
-                    <Check control={control} name={onFocusInput} />
+                    <Validator control={control} name={onFocusInput} />
                 ) : null}
                 <input
                     type="number"
-                    {...register("age")}
+                    {...register("age", {
+                        minLength: {
+                            value: 2,
+                        },
+                        pattern: {
+                            value: /^(1[4-9]|[2-9][0-9]|100)$/,
+                        },
+                    })}
                     placeholder="age"
                     autoComplete="age"
                     onFocus={(item) => setOnFocusInput(item.target.name)}
                     className="www"
                 />
                 {onFocusInput && onFocusInput === "age" ? (
-                    <Check control={control} name={onFocusInput} />
+                    <Validator control={control} name={onFocusInput} />
                 ) : null}
-                <input type="submit" />
+                <input type="submit" value="회원가입" />
             </form>
         </div>
     );
