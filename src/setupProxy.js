fb5d97933aa4e59e1afbb7fd6444b22fafbec53f
KROomoO <parksungik00@naver.com>
2024-03-29 20:58:18 +0900

    aws

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = (app) => {
     app.use(
         createProxyMiddleware("/api", {
-            target: "http://13.124.237.67/",
+            target: "http://13.124.237.67:8080/",
             changeOrigin: true,
             pathRewrite: {
                 "^/api": "",
